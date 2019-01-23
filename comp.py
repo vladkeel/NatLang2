318e2d07bc9841b081b5d36db8ddac500d4a8a07
afik <tkafik@gmail.com>
2019-01-23 12:28:35 +0200

    add comp

--- comp.py
+++ comp.py
@@ -1,10 +1,24 @@
 from ComplexModel import ComplexModel, complex_feature_extractor
+from SimpleModel import SimpleModel, simple_feature_extractor
 from data_parser import parse
 import pickle
 
-with open('data/comp.labeled', 'w') as f:
+
+# Simple model comp
+with open('data/comp_m1_302575287.wtag', 'w') as f:
     comp_data = parse('data/comp.unlabeled')
-    w = pickle.load(open('w_pickle/w_complex_20', 'rb'))
+    w = pickle.load(open('w_pickle/w_simple_100', 'rb'))
+    simple_model = SimpleModel(comp_data, simple_feature_extractor, w)
+    for sentence in comp_data:
+        result = simple_model.infer(sentence)
+        for word in result:
+            f.write("{}\t{}\t_\t{}\t_\t_\t{}\t_\t_\t_\n".format(word.counter, word.token, word.pos, word.head))
+        f.write("\n")
+
+# Complex model comp
+with open('data/comp_m2_302575287.wtag', 'w') as f:
+    comp_data = parse('data/comp.unlabeled')
+    w = pickle.load(open('w_pickle/w_complex_100', 'rb'))
     complex_model = ComplexModel(comp_data, complex_feature_extractor, w)
     for sentence in comp_data:
         result = complex_model.infer(sentence)
